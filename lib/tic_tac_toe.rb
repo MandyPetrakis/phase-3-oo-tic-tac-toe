094db471b2f2bc51a52bff71d34950ac962a689b
Mandy Petrakis <mandypetrakis@gmail.com>
2023-03-23 15:02:48 -0700

    commit tree 426b7b5b1cbf7f756f97a9127c68941f622de900

---
+++ lib/tic_tac_toe.rb
@@ -0,0 +1,48 @@
+class TicTacToe
+    attr_accessor :board
+
+    WIN_COMBINATIONS = [
+        [0,1,2],
+        [3,4,5],
+        [6,7,8],
+        [0,4,8],
+        [2,4,6],
+        [0,3,6],
+        [1,4,7],
+        [2,5,8]
+    ]
+
+    def initialize
+        @board = [" "," "," "," "," "," "," "," "," "]
+    end
+    def display_board 
+        puts " #{@board[0]} | #{@board[1]} | #{@board[2]} "
+        puts "-----------"
+        puts " #{@board[3]} | #{@board[4]} | #{@board[5]} "
+        puts "-----------"
+        puts " #{@board[6]} | #{@board[7]} | #{@board[8]} "
+    end
+    def input_to_index(str)
+        str.to_i - 1
+    end
+    def move(index, token= "X")
+        @board[index] = token
+    end
+    def position_taken?(index)
+        @board[index] == " " ? false : true
+    end
+    def valid_move?(position)
+        if position >= 0 && position <= 8 && @board[position] == " " then true
+        else false
+        end
+    end
+    def turn_count
+        count =0
+        i =0
+        while i < @board.length 
+            if @board[0] == " " then nil
+            else count +=1
+            end
+        count
+    end
+end
